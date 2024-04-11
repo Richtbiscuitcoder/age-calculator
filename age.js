60c7d66bf0f42ae100e114501822f8f2a1abcc33
Rich <richowen87@icloud.com>
2024-04-11 18:19:20 +0100

    nope..

--- age.js
+++ age.js
@@ -1,18 +1,17 @@
 document.querySelector('#calculate').addEventListener('click', ()=>{
-    const day = document.querySelector('.day').value
-    const month = document.querySelector('.month').value
-    const year = document.querySelector('.year').value
+    const dd = document.querySelector('#day').value
+    const mm = document.querySelector('#month').value
+    const yy = document.querySelector('#year').value
 
-    const dob = new Date('day', 'month', 'year')
+    const dob = new Date(yy, mm, yy)
     const today = new Date()
     const age = today - dob
     const years = age / 31536000000
-    const months = ((years - Math.floor(years)) *12)
-    const days = ((months - Math.floor(months))* 30)
-    document.querySelector('#year').textContent = Math.floor(years)
-    document.querySelector('#month').textContent = Math.floor(months)
-    document.querySelector('#day').textContent = Math.floor(days)
-    return dob
+    const months = ((years - Math.floor(years)) * 12)
+    const days = ((months - Math.floor(months)) * 30)
+    document.querySelector('#YY').textContent = Math.floor(years)
+    document.querySelector('#MM').textContent = Math.floor(months)
+    document.querySelector('#DD').textContent = Math.floor(days)
 })
 
 
