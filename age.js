e44be670a628d9c959b3dc7e8fce44a3087396d8
Rich <richowen87@icloud.com>
2024-04-12 05:59:24 +0100

    still not working

--- age.js
+++ age.js
@@ -1,17 +1,17 @@
-document.querySelector('#calculate').addEventListener('click', ()=>{
-    const dd = document.querySelector('#day').value
-    const mm = document.querySelector('#month').value
-    const yy = document.querySelector('#year').value
+document.querySelector('#calculate').addEventListener('click', () => {
+    const dd = document.querySelector('#dd').value
+    const mm = document.querySelector('#mm').value
+    const yy = document.querySelector('#yy').value
 
-    const dob = new Date(yy, mm, yy)
+    const dob = new Date(yy, mm, dd)
     const today = new Date()
     const age = today - dob
     const years = age / 31536000000
     const months = ((years - Math.floor(years)) * 12)
     const days = ((months - Math.floor(months)) * 30)
-    document.querySelector('#YY').textContent = Math.floor(years)
-    document.querySelector('#MM').textContent = Math.floor(months)
-    document.querySelector('#DD').textContent = Math.floor(days)
+    document.querySelector('#years').textContent = Math.floor(years)
+    document.querySelector('#months').textContent = Math.floor(months)
+    document.querySelector('#days').textContent = Math.floor(days)
 })
 
 
