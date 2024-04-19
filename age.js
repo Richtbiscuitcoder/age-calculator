d1faf7189249ad48cbfa5f4fbb781fd3579fa3c6
Rich <richowen87@icloud.com>
2024-04-19 21:06:58 +0100

    comments

--- age.js
+++ age.js
@@ -6,7 +6,7 @@ document.querySelector('#calculate').addEventListener('click', () => {
     const dob = new Date(yy, mm, dd)
     const today = new Date()
     const age = today - dob
-    const years = age / 31536000000
+    const years = age / 31536000000 // 1 year=(365)×(24)×(60)×(60) = 31,536,000seconds
     const months = ((years - Math.floor(years)) * 12)
     const days = ((months - Math.floor(months)) * 30)
     document.querySelector('#years').textContent = Math.floor(years)
